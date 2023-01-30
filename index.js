667ae67478d38829ae7b7efc78b9d54285808758
zinakalu <zina.kalu@gmail.com>
2023-01-30 02:05:46 +0100

    created objects lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+  name: "Lenin",
+  streetAddress: "111 Park Avenue",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+ let updatedEmployee = {...employee};
+ updatedEmployee[key] = value;
+ return updatedEmployee;
+  
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+  let updatedEmployee = {...employee};
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
